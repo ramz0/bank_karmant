64493c726a7a60e70a63bd885a406f98012abe75
Luis Ramírez <97270255+ramz0@users.noreply.github.com>
2024-05-10 11:59:47 -0600

    Delete login.py

--- login.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("hello world")
-=======
-print("brandonnnnn.")
->>>>>>> 494a5f6d6c15c5441197c66f356a20f4ab875fbc
-=======
-print("brandonnnnn.")
->>>>>>> 494a5f6d6c15c5441197c66f356a20f4ab875fbc
