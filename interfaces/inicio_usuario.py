219844342b5234c3179884ee7cfefd4c3f3166ee
ramz0 <luis.syber@gmail.com>
2024-05-11 07:32:38 -0600

    Interfaz del inicio del usuario

--- interfaces/inicio_usuario.py
+++ interfaces/inicio_usuario.py
@@ -1,15 +1,16 @@
 import customtkinter as ctk
 from widgets_custom import DashboardMenuCliente 
 
-
+# LISTA DE LOS BOTONES QUE ESTARAN EN EL MENU:
 nombreBotonesMenu = ['DEPOSITO', 'TRANSFERENCIA', 'RETIRO']
 nombreBotonesMenu2 = ['ACCIONES CUENTA', 'VISUALIZACION\n ADMINISTRADOR', '']
 
 root = ctk.CTk()
-
+# COMO INVOCAR EL MENU DEL USUARIO
 dashboard_menu = DashboardMenuCliente(root, nombreBotonesMenu, 'USUARIO')
 dashboard_menu.grid(row=0, column=0,pady=5, padx=10, ipady=10,ipadx=10, sticky="wsn")
 
+# COMO INVOCAR EL MENU DEL ADMINISTRADOR
 dashboard_menu2 = DashboardMenuCliente(root, nombreBotonesMenu2, 'ADMINISTRADOR')
 dashboard_menu2.grid(row=0, column=1,pady=5, padx=10, ipady=10,ipadx=10, sticky="wsn")
 
