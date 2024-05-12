ae867fb1cb1ba127cfa76b7f622c2d0c0d84f586
ErickMolina222 <molinaerick222@gmail.com>
2024-05-12 13:00:36 -0600

    interfaz de login

--- interfaces/log_in.py
+++ interfaces/log_in.py
@@ -3,50 +3,61 @@ from PIL import Image
 from widgets_custom import DashboardMenuCliente
 
 app = CTk()
-app.geometry("400x400")
+app.geometry("460x400")
 frame_log = CTkFrame(app, fg_color="#2B8C57")
 frame_log.grid(row=0, column=0, sticky= "nswe", pady= 5, padx= 5 )
 
 frame_log_2 = CTkFrame(app)
 frame_log_2.grid(row=0, column=1, sticky= "nswe", pady= 5, padx= 5 )
 
+
 img_logo = Image.open("assets/logobanco.png")
 img_logobanco= CTkImage(light_image=img_logo, dark_image=img_logo, size=(200,130))
 
 lb_logo= CTkLabel(frame_log, image=img_logobanco,text="", compound="center")
-lb_logo.grid(row=0, column=0, sticky= "w",  padx= 15 )
+lb_logo.grid(row=0, column=0, sticky= "we",  padx= 15 )
 
 etiqueta_titulo = CTkLabel(frame_log, text= "Bank-karmant")
 etiqueta_titulo.grid(row=1, column=0)
-#app.wm_attributes('-fullscreen',True)
+
                   
-etiqueta_login = CTkLabel(frame_log_2, text="Inicio de Sesión")
+etiqueta_login = CTkLabel(frame_log_2, text="Inicio de Sesión", anchor="center")
 etiqueta_login.grid(row=0, column=3, pady= 5, padx=5)
 
 etiqueta_usuario = CTkLabel(frame_log_2, text="Ingresa tu usuario")
 etiqueta_usuario.grid(row=1, column=3, pady= 5, padx=5)
 
 entry_nombre_usuario = CTkEntry(frame_log_2)
-entry_nombre_usuario.grid(row=2, column=3, pady= 5, padx=5)
+entry_nombre_usuario.grid(row=2, column=3, pady= 5, padx=20, sticky="we")
 
 etiqueta_pass = CTkLabel(frame_log_2, text="Ingresa tu contraseña")
 etiqueta_pass.grid(row=3, column=3, pady= 5, padx=5)
 
 entry_pass = CTkEntry(frame_log_2)
-entry_pass.grid(row=4, column=3, pady= 5, padx=5)
+entry_pass.grid(row=4, column=3, pady= 5, padx=20, sticky="we")
 
 
 
 boton= CTkButton(frame_log_2, text="Ingresar", corner_radius=32, fg_color="#2B8C57",
                 hover_color="green", border_color="#FFFFFF",
-                border_width=2)
-boton.grid(row=8, column=3, pady= 5, padx= 5 )
+                border_width=2, width=200, height=50 )
+boton.grid(row=5, column=3, pady= 5, padx= 5 )
+
 
 
 app.rowconfigure(0, weight=1)  
-app.columnconfigure(0, weight=10)
+app.columnconfigure(0, weight=6)
+
+app.columnconfigure(1, weight=4)
 
 frame_log.rowconfigure(0, weight=1)
+frame_log_2.columnconfigure(3, weight=1)
+frame_log_2.rowconfigure(0, weight=1)
+
+frame_log_2.rowconfigure(5, weight=3)
+
+
+
 
 app.mainloop()
 
