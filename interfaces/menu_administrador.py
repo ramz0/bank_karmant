39ce1807be93c0f769e91360fbf893a327e32edf
Axel Guzman <axelguzmanfuentes@gmail.com>
2024-05-14 01:17:27 -0600

    actualizar menu_administrador  de Erick

--- interfaces/menu_administrador.py
+++ interfaces/menu_administrador.py
@@ -11,7 +11,7 @@ frame_head=CTkFrame(app, fg_color="#2B8C57")
 frame_head.grid(row=0, column=0, sticky= "ne", pady= 5, padx= 5)
 
 img_logo = Image.open("assets/logobanco.png")
-img_logobanco= CTkImage(light_image=img_logo, dark_image=img_logo, size=(200,150))
+img_logobanco= CTkImage(light_image=img_logo, dark_image=img_logo, size=(200,130))
 
 lb_logo= CTkLabel(frame_head, image=img_logobanco,text="", compound="center")
 lb_logo.grid(row=1, column=0, sticky= "nswe",  padx= 15 )
@@ -19,9 +19,13 @@ lb_logo.grid(row=1, column=0, sticky= "nswe",  padx= 15 )
 etiqueta_titulo = CTkLabel(frame_head, text= "Bank-karmant")
 etiqueta_titulo.grid(row=2, column=0)
 
+def cerrar_sesion():
+    app.destroy()
+    subprocess.Popen(['python', 'log_in.py'])
+
 boton= CTkButton(frame_head, text="Cerrar Sesión", corner_radius=32, fg_color="#2B8C57",
                 hover_color="green", border_color="#FFFFFF",
-                border_width=2, width=150, height=50 )
+                border_width=2, width=150, height=50, command=cerrar_sesion )
 boton.grid(row=1, column=5, pady= 50, padx= 50 )
 
 lb_menu=CTkLabel(frame_head,font=("Helvetica", 50) , text="Menú")
@@ -48,6 +52,8 @@ def visualizacion_administrador():
      # Código para mostrar la interfaz de acciones de cuenta
     app.destroy()
     subprocess.Popen(['python', 'vizualizacion_administrador.py'])
+
+
 
 boton= CTkButton(frame, text="Acciones de Cuenta", corner_radius=32, fg_color="#2B8C57",
                 hover_color="green", border_color="#FFFFFF",
