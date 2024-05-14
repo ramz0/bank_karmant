c59605320c2754d3086bc3d487c06d8458a08c76
Axel Guzman <axelguzmanfuentes@gmail.com>
2024-05-13 19:27:25 -0600

    Conexion a mariadb nombre de base de datos y configuracion

--- interfaces/ConexionMDB.py
+++ interfaces/ConexionMDB.py
@@ -1,15 +1,14 @@
 import mysql.connector
 
-class ConexionMDB():
-    def conectar_a_MariaDB():
-        try:
+def conectar_a_MariaDB():
+    try:
         # Reemplaza los valores de usuario, contraseña, host y nombre de la base de datos con los correctos
-            connection = mysql.connector.connect(
-                user='root',
-                password='',
-                host='localhost',
-                database='bank_karmant'
-            )
-            return connection
-        except mysql.connector.Error as error:
-            raise Exception(f"Error de conexión a MariaDB: {str(error)}")
+        connection = mysql.connector.connect(
+            user='root',
+            password='',
+            host='localhost',
+            database='bank_karmant'
+        )
+        return connection
+    except mysql.connector.Error as error:
+        raise Exception(f"Error de conexión a MariaDB: {str(error)}")
