2da374fa0f99f42e01bc53891596542cd67e1ed7
Chinomixgamer <chinomixgamer@gmail.com>
2024-05-13 21:19:41 -0600

    Guardando cambios antes de cambiar a la rama 'brandon'

--- interfaces/ConexionMDB.py
+++ interfaces/ConexionMDB.py
@@ -1,16 +1,17 @@
 from tkinter import Toplevel
 
-import mysql
+import pymysql
+
 
 class ConexionMDB():
-    def __init__(self, parent):
-        try:
-            connection = mysql.connector.connect(
-                user='admin',
-                password='12345',
-                host='localhost',
-                database='mundial_alemania_2006'
-            )
-            return connection
-        except mysql.connector.Error as error:
-            raise Exception(f"Error de conexión a MariaDB: {str(error)}")
+    def __init__(self):
+        self.connection = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='',
+            db='bank_karmant'
+        )
+
+        self.cursor=self.connection.cursor()
+        self.cursor(print ("conexion establecida con exito"))
+
